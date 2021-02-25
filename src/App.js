abffd258ef5667869ffde0603bc94b7746b76071
justinhilario <justinhilario@gmail.com>
2021-02-25 11:28:28 -0800

    refresh

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      squares: [null, null, null, null, null, null, null, null, null],
       playersTurn: 1,
       playerOne:[],
       playerTwo: [],
@@ -22,26 +22,58 @@ class App extends Component{
         ]
     }
   }
-
+  // push joined numbers into players empty arrays, if theres a winning combo, have an includes that looks for that
   // .some() ???
+  // winner, reset, game state
+
+
+  // so far we are able to index each players moves and differentiate between player turns
+  // figure out endgame
+
+
+  calculateWinner = (squares)  => {
+    const winCombo = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  for (let i = 0; i < winCombo.length; i++) {
+		const [a, b, c] = winCombo[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
+}
 
   handleGamePlay = (index) => {
     const { squares, playersTurn, playerOne, playerTwo, winCombo } = this.state
-    // want 'X' or 'O' to show up per click 
-    // player 1 = X player 2 = O need to switch
     if(playersTurn === 1) {
       squares[index] = '❌'
-      // } if (playerOne.some(value => {
-
-      // } )) {
-      this.setState({squares: squares, playersTurn: playersTurn + 1, playerOne: [...this.state.playerOne, index]})
+          this.setState({squares: squares, playersTurn: playersTurn + 1, playerOne: [...this.state.playerOne, index]})
+          if (this.calculateWinner(squares) !==  null){
+            this.setState({ playersTurn: 0 })
+            setTimeout(function () {alert("YOU WON!")}, 300 )
+          } 
     } else if(playersTurn === 2) {
       squares[index] = '⭕️'
       this.setState({ squares: squares, playersTurn: playersTurn - 1, playerTwo: [...this.state.playerTwo, index]})
+      if (this.calculateWinner(squares) !==  null){
+        this.setState({ playersTurn: 0 })
+        setTimeout(function () {alert("YOU WON!")}, 300 )
+      } 
     }
   }
 
-  
+
+  resetGameboard = () => {
+    
+  }
 
   render(){
     return(
@@ -55,10 +87,11 @@ class App extends Component{
                     index={ index }
                     key={ index }
                     handleGamePlay={ this.handleGamePlay }
-              />
+              />  
             )
           })}
         </div>
+        <button onClick = { this.render}> </button>
       </>
     )
   }
