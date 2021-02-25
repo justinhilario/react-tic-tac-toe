2e43110849fd88ed9d5f27add1d546d9ab02eee9
kevinnarbas <kevinnarbas@gmail.com>
2021-02-24 16:09:53 -0800

    adds gameboard and switch logic

--- src/App.js
+++ src/App.js
@@ -6,15 +6,41 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      playersTurn: 1
     }
   }
+
+  handleGamePlay = (index) => {
+    const { squares, playersTurn } = this.state
+    // want 'X' or 'O' to show up per click 
+    // player 1 = X player 2 = O need to switch
+    if(playersTurn === 1) {
+      squares[index] = '❌'
+      this.setState({ squares: squares, playersTurn: playersTurn + 1})
+    } else if(playersTurn === 2) {
+      squares[index] = '⭕️'
+      this.setState({ squares: squares, playersTurn: playersTurn - 1})
+    }
+  }
+
 
   render(){
     return(
       <>
         <h1>Tic Tac Toe</h1>
-        <Square />
+        <div className='gameboard'>
+          { this.state.squares.map((value, index) => {
+            return (
+              <Square 
+                    value={ value }
+                    index={ index }
+                    key={ index }
+                    handleGamePlay={ this.handleGamePlay }
+              />
+            )
+          })}
+        </div>
       </>
     )
   }
